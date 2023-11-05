896a6960f06c206704865e177c4b2bab12f9b942
razenkovv <razenkovv>
2023-11-05 19:33:00 +0300

    poisson_eq

--- LR4/main.py
+++ LR4/main.py
@@ -1,9 +1,8 @@
 import task as t
 
-
-# t.solve()
-# t.animation("show")
-
+print(f"eps: {t.eps_}, tau: {t.tau_}.")
+t.solve()
+# t.animation()
 savepath = f"{t.h_x}_{t.h_y}.png"
-# t.plot_step("result", "jet", savepath)
+t.plot_step("result", "jet", savepath)
 # t.plot_exact("jet", "exact_sol.png")
--- LR4/task.py
+++ LR4/task.py
@@ -4,57 +4,56 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, writers
 import time
 import os
+import multiprocessing as mp
+from multiprocessing.shared_memory import SharedMemory
 
 from tdma import tdma
 
-
 L_x = np.sqrt(2)
 L_y = 1
-h_x = 0.01
-h_y = 0.01
+h_x = 0.1
+h_y = 0.1
 eps_ = 1e-6
 
 lambda_min = (np.pi / L_x) ** 2 + (np.pi / L_y) ** 2
 lambda_max = 4 / h_x ** 2 + 4 / h_y ** 2
 tau_ = 1 / np.sqrt(lambda_min * lambda_max)
-print(f"eps: {eps_}. tau: {tau_}.")
 
 path = "D:\\мифи\\7_семестр\\численные_методы\\лабораторные\\LR4\\data"
 steps_per_save = 10
 
 
 def phi1(y):
-    #return 15*y*np.cos(2*y) + 3*y**2
-    #return 5 - 4 * y
-    #return np.sqrt(2) * y ** 2 - y ** 3
-    return y**3 + np.cos(3*y)
-
+    # return 15*y*np.cos(2*y) + 3*y**2
+    # return 5 - 4 * y
+    # return np.sqrt(2) * y ** 2 - y ** 3
+    return y ** 3 + np.cos(3 * y)
 
 
 def phi2(y):
-    #return 5 * (3*y - 2) * np.cos(3 + 2*y) + 3*y**2
-    #return 5 - 4 * y
+    # return 5 * (3*y - 2) * np.cos(3 + 2*y) + 3*y**2
+    # return 5 - 4 * y
     # return -np.cos(np.sqrt(2) * np.pi * y) + 1
-    return -2 + y**3 + np.cos(8*np.sqrt(2) + 3*y)
+    return -2 + y ** 3 + np.cos(8 * np.sqrt(2) + 3 * y)
 
 
 def psi1(x):
-    #return -10*x*np.cos(3*x)
-    #return 5
+    # return -10*x*np.cos(3*x)
+    # return 5
     # return np.sqrt(2) * x ** 2 - x ** 3
-    return -x**2 + np.cos(8*x)
+    return -x ** 2 + np.cos(8 * x)
 
 
 def psi2(x):
-    #return 5 * (6-2*x)*np.cos(3*x + 4) + 12
-    #return 1
-    #return -np.cos(np.sqrt(2) * np.pi * x) + 1
-    return -x**2 + 1 + np.cos(8*x + 3)
+    # return 5 * (6-2*x)*np.cos(3*x + 4) + 12
+    # return 1
+    # return -np.cos(np.sqrt(2) * np.pi * x) + 1
+    return -x ** 2 + 1 + np.cos(8 * x + 3)
 
 
 def f(x, y):
-    #return (-65*np.cos(3*x+2*y)*(-2*x+3*y)+6)
-    #return 0
+    # return (-65*np.cos(3*x+2*y)*(-2*x+3*y)+6)
+    # return 0
     # return x * y
     return -2 - 73 * np.cos(8 * x + 3 * y) + 6 * y
 
@@ -63,14 +62,12 @@ def solve(Lx=L_x, Ly=L_y, hx=h_x, hy=h_y, tau=tau_, eps=eps_):
     Nx = np.ceil((Lx / hx)).astype(int)
     Ny = np.ceil((Ly / hy)).astype(int)
     mesh = np.zeros(shape=(Ny, Nx))
-    mesh_prev = np.copy(mesh)
     xaxis = np.linspace(0.0, Lx, Nx)
     yaxis = np.linspace(0.0, Ly, Ny)
     mesh[:, 0] = phi1(yaxis)
     mesh[:, -1] = phi2(yaxis)
     mesh[0, :] = psi1(xaxis)
     mesh[-1, :] = psi2(xaxis)
-    mesh_prev[1:-1, 1:-1] = 0
 
     right1 = np.zeros(Nx - 2)
     left1 = np.zeros((Nx - 4) * 3 + 4)
@@ -95,26 +92,28 @@ def solve(Lx=L_x, Ly=L_y, hx=h_x, hy=h_y, tau=tau_, eps=eps_):
             left1[-2] = left1[1]
             right1[-1] = mesh[i, -1] / hx ** 2 + mesh[i, -2] / tau + (mesh[i, -3] - 2 * mesh[i, -2] + mesh[i, -1]) / hx ** 2 - f(xaxis[-2], yaxis[i])
             j = 2
+            k = 1
             while j < (Nx - 4) * 3 + 2:
                 left1[j:j + 3] = [-1 / hx ** 2, 1 / tau + 2 / hx ** 2, -1 / hx ** 2]
+                right1[k] = mesh[i, k + 1] / tau + (mesh[i, k + 2] - 2 * mesh[i, k + 1] + mesh[i, k]) / hx ** 2 - f(xaxis[k + 1], yaxis[i])
                 j += 3
-            for j in range(1, Nx - 3):
-                right1[j] = mesh[i, j + 1] / tau + (mesh[i, j + 2] - 2 * mesh[i, j + 1] + mesh[i, j]) / hx ** 2 - f(xaxis[j + 1], yaxis[i])
+                k += 1
             mesh[i, 1:-1] = tdma(left1, right1)
 
         for i in range(1, Nx - 1):
             left2[0] = 1 / tau + 2 / hy ** 2
             left2[1] = -1 / hy ** 2
             right2[0] = mesh[0, i] / hy ** 2 + mesh[1, i] / tau + (mesh[2, i] - 2 * mesh[1, i] + mesh[0, i]) / hy ** 2 - f(xaxis[i], yaxis[1])
-            left2[-1] = left1[0]
-            left2[-2] = left1[1]
+            left2[-1] = left2[0]
+            left2[-2] = left2[1]
             right2[-1] = mesh[-1, i] / hy ** 2 + mesh[-2, i] / tau + (mesh[-3, i] - 2 * mesh[-2, i] + mesh[-1, i]) / hy ** 2 - f(xaxis[i], yaxis[-2])
             j = 2
+            k = 1
             while j < (Ny - 4) * 3 + 2:
                 left2[j:j + 3] = [-1 / hy ** 2, 1 / tau + 2 / hy ** 2, -1 / hy ** 2]
+                right2[k] = mesh[k + 1, i] / tau + (mesh[k + 2, i] - 2 * mesh[k + 1, i] + mesh[k, i]) / hy ** 2 - f(xaxis[i], yaxis[k + 1])
                 j += 3
-            for j in range(1, Ny - 3):
-                right2[j] = mesh[j + 1, i] / tau + (mesh[j + 2, i] - 2 * mesh[j + 1, i] + mesh[j, i]) / hy ** 2 - f(xaxis[i], yaxis[j + 1])
+                k += 1
             mesh[1:-1, i] = tdma(left2, right2)
 
         curr_eps = np.max(np.abs(mesh - mesh_prev))
@@ -154,7 +153,7 @@ def plot_step(s, cmap, file, show=False):
     fig.savefig(f'images/{file}')
 
 
-def animation(param):
+def animation(param="save"):
     files = [int(os.path.splitext(f_)[0]) for f_ in os.listdir(path)]
     files.sort()
     files = [str(f) + '.csv' for f in files]
@@ -185,8 +184,8 @@ def animation(param):
 
 def plot_exact(cmap, savepath):
     def exact(x, y):
-        return -x**2 + y**3 + np.cos(-8*x - 3*y)
-        #return 5*(-2*x+3*y)*np.cos(3*x+2*y)+3*y**2
+        return -x ** 2 + y ** 3 + np.cos(-8 * x - 3 * y)
+        # return 5*(-2*x+3*y)*np.cos(3*x+2*y)+3*y**2
 
     acc = 1000
     x = np.linspace(0, L_x, acc)
